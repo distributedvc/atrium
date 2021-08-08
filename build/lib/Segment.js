89806c84b67fd11c72d6ac494ef1550949c8f988
Gomah <hi@gomah.fr>
2021-08-08 12:02:58 +1000

    chore(): ignore build folder

--- build/lib/Segment.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.analytics = void 0;
-const analytics_node_1 = require("@distributed/analytics-node");
-exports.analytics = new analytics_node_1.Analytics(process.env.SEGMENT_ANALYTICS_KEY);
